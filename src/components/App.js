b03a84e6ec8472779fdb7451d3a3bc41c37779b5
shivpujari <117809630+shivpujari@users.noreply.github.com>
2023-03-14 10:39:10 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,39 +1,34 @@
-import React, {Component, useState} from "react";
-import '../styles/App.css';
+import React, { Component, useState } from "react";
+import "../styles/App.css";
 import { Link, Route, Switch, useLocation } from "react-router-dom";
-import LocationDisplayjs from "./LocationDisplay";
-
+import LocationDisplayComp from "./LocationDisplay";
 function About() {
-  return <div>This is about page.</div>;
+  return <div>You are on the about page.</div>;
 }
-
 function Home() {
-  return <div>This is Home page.</div>;
+  return <div>You are home.</div>;
 }
 
 function Invalid() {
-  return <div>No matches Found</div>;
+  return <div>No match</div>;
 }
-class App extends Component {
-    render() {
 
-        return(
-            <div id="main">
-               
-    <Link to="/">Home</Link>
-    <Link to="/about">About</Link>
- <Switch>
+class App extends Component {
+  render() {
+    return (
+      <div id="main">
+        <Link to="/">Home</Link>
+        <Link to="/about">About</Link>
+        <Switch>
           <Route path="/" exact component={Home} />
           <Route path="/about" component={About} />
           <Route path="/" component={Invalid} />
         </Switch>
-        <LocationDisplayjs/>
-
-            </div>
-        )
-    }
+        <LocationDisplayComp />
+      </div>
+    );
+  }
 }
 
-
 export default App;
-export const LocationDisplay = LocationDisplayjs;
+export const LocationDisplay = LocationDisplayComp;
